ea074f781010b303124d97430d058e1348bf09e6
dave <lklone2005@gmail.com>
2021-02-01 01:51:03 +0900

    Inherit templates and apply html, css code snippets

--- blog/views.py
+++ blog/views.py
@@ -32,8 +32,9 @@ def about(request):
     # return HttpResponse('<h1>Blog About</h1>')
     return render(request, 'blog/about.html', {'title' : 'About'})
 
-
-# 모든 뷰에 동일한 태그가 중복되서 뿌려질 이유가 없다.
-# 따라서 모든 뷰에 대해 반복되는 html를 만들어야 한다. 가장 좋은 방법은 템플릿을 사용하는 것이다.
-# blog -> template -> blog -> template.html
-# http 대신에 렌더링된 템플릿을 response할 수 있다.
+'''
+    모든 뷰에 동일한 태그가 중복되서 뿌려질 이유가 없다.
+    따라서 모든 뷰에 대해 반복되는 html를 만들어야 한다. 가장 좋은 방법은 템플릿을 사용하는 것이다.
+    blog -> template -> blog -> template.html
+    http 대신에 렌더링된 템플릿을 response할 수 있다.
+'''
