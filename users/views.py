af1eb114563ea0f54bb4f956125a290dca37f867
dave <lklone2005@gmail.com>
2021-02-06 15:18:10 +0900

    Update User Profile

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 '''
     from django.contrib.auth.forms import UserCreationForm
@@ -38,7 +38,31 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        '''
+            사용자가 새로운 프로필을 작성하고 서버에 보낼 때
+            request.POST, request.FILES는 그 양식을 보존해서 u_form, p_form으로 전달
+        '''
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'u_form' : u_form,
+        'p_form' : p_form
+    }
+
+    return render(request, 'users/profile.html', context)
 
 '''
     messages 에러에 사용할 수 있는 method
