c724f4b97d3a0823d2d9ae29d403938ac368f29e
dave <lklone2005@gmail.com>
2021-02-06 18:20:21 +0900

    Change function-based View to class-based View at home()

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from .views import PostListView
 
+# url을 매핑하는데 있어서 정규표현식은 복잡하기 때문에 사용하지 않는 것이 좋다
 urlpatterns = [
-    # url을 매핑하는데 있어서 정규표현식은 복잡하기 때문에 사용하지 않는 것이 좋다
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    # -> blog/post_list.html을 찾고있다
+    # <app>/<model>_<viewtype>.html
     path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
 
 def home(request):
     context = {
         'posts': Post.objects.all()
     }
+
+    print("This is Post.object", Post.objects.all())
+
     # return HttpResponse('<h1>Blog Home</h1>')
     # 템플릿을 '참조'하는 방식으로 렌더링하려는 템플릿 이름
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'    # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
 
 
 def about(request):
@@ -20,4 +30,10 @@ def about(request):
     따라서 모든 뷰에 대해 반복되는 html를 만들어야 한다. 가장 좋은 방법은 템플릿을 사용하는 것이다.
     blog -> template -> blog -> template.html
     http 대신에 렌더링된 템플릿을 response할 수 있다.
+'''
+
+'''
+    Part 10 - Create, Update, and Delete Posts
+    지금까지는 함수 기반 View를 사용했지만 class기반 View를 사용해보자.
+    클래스 기반 View는 함수기반보다 기본 제공되는 기능이 더 많다.
 '''
